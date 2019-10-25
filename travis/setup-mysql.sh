ce2461a7fad536b0f300b05aed026c9e2a5ed598
Nikita Popov <nikita.ppv@gmail.com>
2019-10-25 15:16:38 +0200

    Fix condition

--- travis/setup-mysql.sh
+++ travis/setup-mysql.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo $TRAVIS_ARCH_NAME
-if [ "$TRAVIS_ARCH_NAME" = "arm64" ]; then
+if [ -n "$ARM64" ]; then
   sudo mysql -e 'CREATE USER "travis"@"localhost" IDENTIFIED BY ""'
   sudo mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "travis"@"localhost"'
 fi
