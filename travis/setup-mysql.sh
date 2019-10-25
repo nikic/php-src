04d02ef5375bc71687445d0df73f24ac6366fe20
Nikita Popov <nikita.ppv@gmail.com>
2019-10-25 15:07:49 +0200

    Create user only on arm64

--- travis/setup-mysql.sh
+++ travis/setup-mysql.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo mysql -e 'CREATE USER "travis"@"localhost" IDENTIFIED BY ""'
-sudo mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "travis"@"localhost"'
+echo $TRAVIS_ARCH_NAME
+if [ "$TRAVIS_ARCH_NAME" = "arm64" ]; then
+  sudo mysql -e 'CREATE USER "travis"@"localhost" IDENTIFIED BY ""'
+  sudo mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "travis"@"localhost"'
+fi
 mysql -e "CREATE DATABASE IF NOT EXISTS test"
