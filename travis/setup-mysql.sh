84f139b07e22a2a272f0cf2167336b2557f603a8
Nikita Popov <nikita.ppv@gmail.com>
2019-10-25 14:49:37 +0200

    Finish

--- travis/setup-mysql.sh
+++ travis/setup-mysql.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo mysql -e 'CREATE USER "travis"@"localhost" IDENTIFIED BY ""'
+sudo mysql -e 'GRANT ALL PRIVILEGES ON *.* TO "travis"@"localhost"'
 mysql -e "CREATE DATABASE IF NOT EXISTS test"
